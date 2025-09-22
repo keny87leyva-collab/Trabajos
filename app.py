c8e920f319a429c2fbb179540885d7ed025cabf7
KENY YASSER PANIAGUA LEYVA <keny87leyva@gmail.com>
2025-09-22 08:29:09 -0600

    feat: suma

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ IPAddr=socket.gethostbyname(hostname)
 print(f"IP ADDRESS: {IPAddr}")
 for i in range(10):
     print(f"count: {i}")
+numero_a= input("dame el primer numero: ")
+numero_b=input("dame el segundo numero: ")
+resultado=numero_a+numero_b
+print (resultado)
