b0a5548a03072c405e1b708640a5ce3a3393606d
KENY YASSER PANIAGUA LEYVA <keny87leyva@gmail.com>
2025-09-23 21:07:40 -0600

    Corregí operaciones: conversión a int y operaciones básicas

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ IPAddr=socket.gethostbyname(hostname)
 print(f"IP ADDRESS: {IPAddr}")
 for i in range(10):
     print(f"count: {i}")
-numero_a= input("dame el primer numero: ")
-numero_b=input("dame el segundo numero: ")
-resultado=numero_a+numero_b
-print (resultado)
-print(f"la resta de los numeros es: {int(numero_a)}- {int(numero_b)}")
-print (resultado)
-print(f"multiplicacion {int(numero_a)} * {int(numero_b)}")
+numero_a = int(input("Dame el primer numero: "))
+numero_b = int(input("Dame el segundo numero: "))
+suma = numero_a + numero_b
+resta = numero_a - numero_b
+multiplicacion = numero_a * numero_b
+print(f"La suma de los numeros es: {suma}")
+print(f"La resta de los numeros es: {resta}")
+print(f"La multiplicación de los numeros es: {multiplicacion}")
